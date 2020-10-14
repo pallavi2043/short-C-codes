ea76bd981e533911a2b40c4c4f8654923993221a
pallavi2043 <72869873+pallavi2043@users.noreply.github.com>
2020-10-14 07:01:31 -0700

    Update area of triangle.c

--- area of triangle.c
+++ area of triangle.c
@@ -2,8 +2,12 @@
 #include<math.h>
 void main(){
 double s,a,b,c,area;
-printf("enter values of a,b,c");
-scanf("%f%f%f",&a,&b,&c);
+printf("enter values of a");
+scanf("%f",&a);
+  printf("enter the value of b");
+  scanf("%f",&b);
+  printf("enter the value of c");
+  scanf("%f",&c);
 s=(a+b+c)/2;
 area=sqrt(s*(s-a)*(s-b)*(s-c));
 printf("area is=%f",area);
